ff1f460a2a739c72859e37e01ea102de9a0f569d
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2022-04-08 00:58:45 +0200

    Added A* code

---
+++ Assets/Scripts/Grupo 12/AStarMind.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.DataStructures;
+using UnityEngine;
+
+namespace Assets.Scripts.SampleMind
+{
+    public class AStarMind : AbstractPathMind {
+        private Stack<Locomotion.MoveDirection> currentPlan = new Stack<Locomotion.MoveDirection>();
+
+        public override void Repath()
+        {
+            currentPlan.Clear();
+        }
+
+        public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
+        {
+            //Si aún quedan pasos en el plan, los devuelve
+            if (currentPlan.Count > 0) 
+            {
+                return currentPlan.Pop();
+            }
+
+            //Busca un plan para seguir
+            var searchResult = Search(boardInfo, currentPos, goals);
+
+            //Si no se encuentra el plan, no solicita ningún movimiento
+            if (searchResult == null)
+            {
+                Debug.Log("Cannot find a path");
+                return Locomotion.MoveDirection.None;
+            }
+
+            //Si encuentra un plan, lo va añadiendo a la pila desde la meta hasta el inicio
+            while (searchResult.Parent != null)
+            {
+                currentPlan.Push(searchResult.ProducedBy);
+                searchResult = searchResult.Parent;
+            }
+
+            //Al sacar los elementos en orden inverso, realiza el plan desde el inicio
+            if (currentPlan.Count > 0)
+                return currentPlan.Pop();
+
+            return Locomotion.MoveDirection.None;
+        }
+
+        private Node<Locomotion.MoveDirection> Search(BoardInfo board, CellInfo start, CellInfo[] goals)
+        {
+            //Crea una lista vacía de nodos
+            var open = new List<Node<Locomotion.MoveDirection>>();
+
+            //Se crea el nodo inicial y se añade a la lista abierta
+            var n = new Node<Locomotion.MoveDirection>(board, start, goals);
+            open.Add(n);
+
+            //Mientras la lista no esté vacía
+            while (open.Any())
+            {
+                //Se mira el primer nodo de la lista, se saca de la lista y se comprueba que
+                //sea el nodo destino
+                var node = open[0];
+                open.RemoveAt(0);
+
+                //Si es el nodo destino, se devuelve el nodo que contiene el plan
+                if (node.isSameNode(goals[0]))
+                    return node;
+
+                if (node.Cost == float.MaxValue)
+                    return null;
+
+                //Expande el nodo a sus vecinos (calcula coste de cada uno, etc) y los añade en la lista
+                var nodesToAdd = node.ExpandNode();
+                for (int i = 0; i < nodesToAdd.Count; i++)
+                {
+                    if (nodesToAdd[i] == null) continue; //ExpandNode puede devolver nulos, los desechamos
+
+                    //Asignamos la dirección que debe seguir y lo añadimos a la lista
+                    if (nodesToAdd[i].dir == 0)
+                        nodesToAdd[i].ProducedBy = Locomotion.MoveDirection.Up;
+                    if (nodesToAdd[i].dir == 1)
+                        nodesToAdd[i].ProducedBy = Locomotion.MoveDirection.Right;
+                    if (nodesToAdd[i].dir == 2)
+                        nodesToAdd[i].ProducedBy = Locomotion.MoveDirection.Down;
+                    if (nodesToAdd[i].dir == 3)
+                        nodesToAdd[i].ProducedBy = Locomotion.MoveDirection.Left;
+                    open.Add(nodesToAdd[i]);
+                }
+
+                //Ordena la lista para expandir el nodo más prometedor
+                open.Sort();
+            }
+
+            //Si se vacía la lista y no ha encontrado el nodo destino
+            //quiere decir que no se ha encontrado un plan
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Grupo 12/Node.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Assets.Scripts.DataStructures
+{
+    //Clase Node genérica, hereda de IComparable para poder ser ordenada automáticamente
+    //por una lista genérica al sobreescribir el método CompareTo
+    public class Node<T> : IComparable<Node<T>>
+    {
+        public Node<T> Parent { get; set; } //Nodo padre de este nodo
+        public T ProducedBy; //Dato genérico que ha producido la expansión del nodo
+        public float Cost { get; set; } //Coste de acceder a un nodo
+        public int dir { get; private set; } //Traducción de entero a dato genérico (dirección)
+
+        private float manhDist; //Distancia Manhattan a un nodo (func. heurística)
+        
+        private CellInfo cell; //Celda asociada al nodo
+        private CellInfo[] goals; //Array de metas a seguir
+        private BoardInfo board; //Información del tablero
+
+        public Node(){
+            this.Parent = null;
+            this.cell = null;
+            this.board = null;
+            this.goals = null;
+            this.manhDist = 0.0f;
+        }
+
+        public Node(BoardInfo board, CellInfo cell, CellInfo[] goals)
+        {
+            this.Parent = null;
+            this.cell = cell;
+            this.board = board;
+            this.goals = goals;
+
+            //Calculamos la distancia Manhattan como *m = |x - x0| + |y - y0|*
+            manhDist = Mathf.Abs(goals[0].RowId - cell.RowId) + Mathf.Abs(goals[0].ColumnId - cell.ColumnId);
+        }
+
+        //Comprueba si dos nodos tienen la misma celda asociada
+        public bool isSameNode(CellInfo c)
+        {
+            bool isEqual = true;
+            if (c.RowId != cell.RowId || c.ColumnId != cell.ColumnId) isEqual = false;
+            return isEqual;
+        }
+
+        //Expande el nodo, comprobando qué direcciones son transitables
+        public List<Node<T>> ExpandNode()
+        {
+            //Devuelve los vecinos del escenario, sean transitables o no
+            CellInfo[] neighbours = cell.GetNeighbours(board);
+            List<Node<T>> nodes = new List<Node<T>>();
+
+            //Asigna a cada nodo una celda asociada y calcula su coste
+            int i = 0;
+            foreach (CellInfo c in neighbours)
+            {
+                if (c == null) //Desechamos los vecinos nulos
+                {
+                    i++;
+                    continue;
+                }
+
+                Node<T> newNode = new Node<T>(board, c, goals);
+                newNode.Parent = this; //Asignamos como padre de cada nodo expandido, el nodo que los expandió
+
+                var nodeToTraverse = newNode;
+                int depth = 0; // Representa el coste G de llegar a un nodo
+                while (nodeToTraverse != null)
+                {
+                    nodeToTraverse = nodeToTraverse.Parent;
+                    depth++;
+                }
+
+                newNode.Cost = depth + c.WalkCost + manhDist;
+
+                newNode.dir = i;
+
+                nodes.Add(newNode);
+
+                i++;
+            }
+
+            return nodes;
+        }
+
+        //Sobreescribe el método CompareTo de IComparable<T>
+        public int CompareTo(Node<T> node)
+        {
+            if (node == null) //Asignamos los nodos nulos como más grandes
+                return 1; //this node is greater than parameter node
+            int c = Cost.CompareTo(node.Cost);
+
+            return c;
+        }
+    }
+}
